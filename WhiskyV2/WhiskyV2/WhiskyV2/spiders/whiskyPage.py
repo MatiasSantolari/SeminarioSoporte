d65035827f2064a96ccd29917a0a2786bf299382
Matias <santolarimatias@gmail.com>
2021-08-25 19:32:28 -0300

    correccion whiskypage

--- WhiskyV2/WhiskyV2/WhiskyV2/spiders/whiskyPage.py
+++ WhiskyV2/WhiskyV2/WhiskyV2/spiders/whiskyPage.py
@@ -21,21 +21,7 @@ class wSpider(scrapy.Spider):
             wv2.add_css('link', 'a.product-item-link::attr(href)')
             wv2.add_value('listed', webpage)
             yield wv2.load_item()
-<<<<<<< Updated upstream
-=======
 
->>>>>>> Stashed changes
-            try:
-                siguiente = response.css('a.action.next').attrib['href']
-                if siguiente is not None:
-                    yield response.follow(siguiente, callback=self._parse)
-<<<<<<< Updated upstream
-            except Exception:
-                pass  # esta excepcion se genera porque no encuetra el boton de pagina siguiente que se esta asignando
-=======
-            except Exception as e:
-                if 'https://www.whiskyshop.com/spirits' not in response.url:
-                    yield response.follow('https://www.whiskyshop.com/spirits', callback=self._parse)
-                else:
-                    yield response.follow('https://www.whiskyshop.com/world-whiskies', callback=self._parse)
->>>>>>> Stashed changes
+            siguiente = response.css('a.action.next').attrib['href']
+            if siguiente is not None:
+                yield response.follow(siguiente, callback=self._parse)
